f6eb8892dcbdbb757e4e7617a19b1b9a29169052
GdCMistho <giovanni@mistho.io>
2022-05-25 18:06:54 +0200

    Added config file

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+    ITA: {
+        FIRST: 1,
+        DAYS: ['domenica', 'lunedì', 'martedì', 'mercoledì', 'giovedì', 'venerdì', 'sabato'],
+        MONTHS: ['gennaio', 'febbraio', 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre'],
+    },
+    ENG: {
+        FIRST: 0,
+        DAYS: ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'],
+        MONTHS: ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'],
+    }
+};
+
+module.exports = config;
